bffc12653ca3f6f5adca7956ed650bb62f7d934a
ALDRIN LIJO E M <147630494+ALDRIN1704@users.noreply.github.com>
2026-09-23 12:56:33 +0530

    Update default DB_PASSWORD in DB_CONFIG
    Change default DB_PASSWORD to an empty string.

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ else:
         "host": os.getenv("DB_HOST", "localhost"),
         "port": int(os.getenv("DB_PORT", "3306")),
         "user": os.getenv("DB_USER", "root"),
-        "password": os.getenv("DB_PASSWORD", "1704@Aldrin"),
+        "password": os.getenv("DB_PASSWORD", ""),
         "database": os.getenv("DB_NAME", "url_shortener")
     }
 
